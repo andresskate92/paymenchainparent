257d795ba09d29a3bbd65b2530b28eff460dad68
Milton Andrés Cabrera Lopez <andres_pro2009@hotmail.com>
2023-07-13 16:52:12 -0500

    Update Repository [13072023]

--- businessdomain/products/src/main/java/com/macl/ProductsApplication.java
+++ businessdomain/products/src/main/java/com/macl/ProductsApplication.java
@@ -1,7 +1,10 @@
 package com.macl;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * * @author ${milton.cabrera} on ${DATE} - ${TIME}
@@ -9,8 +12,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * * @version 1.0
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductsApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductsApplication.class, args);
+    }
+
+    @Bean
+    public GroupedOpenApi openApi() {
+        return GroupedOpenApi.builder().
+                group("").packagesToScan("com.macl"). build();
     }
 }
